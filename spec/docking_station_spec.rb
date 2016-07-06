2fe44f33b6780e9b572d45a7a9ba3abd7b7fa7fe
William Osborne <wosborne@live.co.uk>
2016-07-06 13:48:35 +0100

    Method releases working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+    it 'release working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+  end
 end
