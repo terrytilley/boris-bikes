b0751d50636489ea447eb577fca18ff5be2484cd
JAstbury <jess.astbury@hotmail.com>
2016-07-06 15:40:36 +0100

    Adds method and tests for docking a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,17 @@ require 'docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-    it 'release working bikes' do
+  it { is_expected.to respond_to :dock_bike }
+  it { is_expected.to respond_to :bike }
+
+  it 'release working bikes' do
       bike = subject.release_bike
       expect(bike).to be_working
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
   end
 end
