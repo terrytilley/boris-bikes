79564d142ee31f1292e959ba57109a09cc3120b4
Terry <terry@terrytilley.com>
2016-07-06 17:31:23 +0100

    releases working bike test fixed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
     expect(bike).to be_working
   end
