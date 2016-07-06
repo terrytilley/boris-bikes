97bcc76474aaa1365696931b75fab83f787c2246
ebere10 <ebere10@hotmail.com>
2016-07-06 15:31:14 +0100

    using instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
 
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
   end
 
 end
